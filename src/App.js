ede305f9b73cc5669d759d894c29b8dcf56f644e
bmaselli13 <brian.maselli@gmail.com>
2023-09-11 12:48:50 -0700

    Refactored entire project starting fresh using components and components folder

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
-import React, { useState } from "react";
-import "./App.css";
-import Post from "./Post";
-import CreatePostForm from "./CreatePostForm";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  const handleNewPost = (newPost) => {
-    setPosts([newPost, ...posts]);
-  };
-
-  const handleLike = (likedPost) => {
-    const updatedPosts = posts.map((post) =>
-      post === likedPost
-        ? { ...post, status: post.status === "like" ? "" : "like" }
-        : post
-    );
-    setPosts(updatedPosts);
-  };
-
-  const handleDislike = (dislikedPost) => {
-    const updatedPosts = posts.map((post) =>
-      post === dislikedPost
-        ? { ...post, status: post.status === "dislike" ? "" : "dislike" }
-        : post
-    );
-    setPosts(updatedPosts);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>SocialFeed</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-
-      <CreatePostForm onNewPost={handleNewPost} />
-
-      {posts.map((post, index) => (
-        <Post
-          key={index}
-          post={post}
-          onLike={handleLike}
-          onDislike={handleDislike}
-        />
-      ))}
     </div>
   );
 }
